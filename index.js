2ce7d3b693d48a1c15095c8a429180988807c26c
Luke Chen <luke.chen.2010@gmail.com>
2020-08-02 01:45:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const num1 = 62
 const num2 = 1
 const multiply = num1 * num2
 
-const random = Math.floor( Math.random() * 1000 ) + 1
+const random = Math.floor(Math.random() * 10) + 1
 
 const mod = 16 % 6
 const max = Math.max( 5, 10, 15, 20 )
